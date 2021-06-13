68ed6ee9520ad61b376fe01ceee27b7cda367829
krish kashiwala <krishkashiwala@gmail.com>
2021-06-13 08:38:46 +0530

    router protected and auth verification remai

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css';
-import { Route, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import HomePage from './HomePage'
 import Register from './Register';
 import Login from './Login';
@@ -10,12 +10,18 @@ function App() {
   return (
     <div>
       <Router>
+        <Switch>
+          <Route path='/' exact component={HomePage} />
+          <Route path='/register' exact component={Register} />
+          <Route path='/login' exact component={Login} />
+          <Route path='/afterhome/addcrop/:id' exact component={AddCrop} />
+          <Route path='/afterhome/:id' exact component={AfterHome} />
+          <Route path="*" component={() => "<h1>404 not found</h1>"} />
+        </Switch>
 
-        <Route path='/' exact component={HomePage} />
-        <Route path='/register' exact component={Register} />
-        <Route path='/login' exact component={Login} />
-        <Route path='/afterhome/addcrop/:id' exact component={AddCrop} />
-        <Route path='/afterhome/:id' exact component={AfterHome} />
+
+        {/* if no exact route found this page is displayed         */}
+        {/* <Route component = {any}/> */}
       </Router>
     </div>
   );
